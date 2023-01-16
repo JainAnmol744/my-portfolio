80df4b5ef02ec7cd22093d8015356645fb2328ed
JainAnmol744 <jainanmol744@gmail.com>
2023-01-16 18:54:48 +0530

    my-portfolio

---
+++ src/components/Footer.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import {SiGmail} from "react-icons/si";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Anmol Jain</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} SB</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/JainAnmol744"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/anmol-jain-a69b5a1a6/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/jain_anmol744"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+            <li className="social-icons">
+                <a
+                  href="mailto:jainanmol744@gmail.com"
+                  style={{ color: "white" }}
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                >
+                  <SiGmail/>
+                </a>
+              </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import weather from '../../Assets/Projects/weather.jpg';
+import inotebook from '../../Assets/Projects/inotebook.jpg';
+import news from '../../Assets/Projects/news.jpg';
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={weather}
+              isBlog={false}
+              title="Weather-App"
+              description=" A weather forecast site, in which user have to enter the city name and it will display the weather conditions of a city. It is built using express, EJS and OpenWeatherMapAPI. "
+              ghLink="https://github.com/JainAnmol744/Weather-App"
+              demoLink="https://weather-app-three-eta-50.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={inotebook}
+              isBlog={false}
+              title="iNotebook-App"
+              description="A Notebook site, to record your note items.It is built using express framework, EJS and it uses MongoDB Atlas as its database. Offers CRUD operations.Login and Signup functionality provides security to your notes. Only authorize user can access notes."
+              ghLink="https://github.com/JainAnmol744/iNotebook-App"
+              demoLink="https://i-notebook-app.vercel.app/login"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={news}
+              isBlog={false}
+              title="News-Portal"
+              description="A daily journal news website. It is built on React framework and EJS and NEWSAPI."
+              ghLink="https://github.com/JainAnmol744/News-App"
+              demoLink="news-app-tau-ruby.vercel.app"              
+            />
+          </Col>
+
+          
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
